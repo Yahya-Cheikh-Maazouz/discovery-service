090014da99e500aa1887ef3537afd7503a6d478b
Yahya-Cheikh-Maazouz <ycheikhmaazouz@xperlean.com>
2022-07-07 14:29:47 +0100

    Dev discovery service

---
+++ src/test/java/com/maazouz/discovery/EurkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maazouz.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
